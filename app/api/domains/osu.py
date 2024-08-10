8e8db4d60ca313be75e89fa60f1fdb3f4146d25c
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2024-08-10 18:23:32 +0000

    Fixed osu direct for new API

--- app/api/domains/osu.py
+++ app/api/domains/osu.py
@@ -410,18 +410,18 @@ async def lastFM(
 # bancho.py supports both cheesegull mirrors & chimu.moe.
 # chimu.moe handles things a bit differently than cheesegull,
 # and has some extra features we'll eventually use more of.
-USING_CHIMU = "chimu.moe" in app.settings.MIRROR_URL
+# USING_CHIMU = "chimu.moe" in app.settings.MIRROR_URL
 
 DIRECT_SET_INFO_FMTSTR = (
-    "{{{setid_spelling}}}.osz|{{Artist}}|{{Title}}|{{Creator}}|"
-    "{{RankedStatus}}|10.0|{{LastUpdate}}|{{{setid_spelling}}}|"
-    "0|{{HasVideo}}|0|0|0|{{diffs}}"  # 0s are threadid, has_story,
+    "{{{setid_spelling}}}.osz|{{artist}}|{{title}}|{{creator}}|"
+    "{{ranked}}|10.0|{{last_updated}}|{{{setid_spelling}}}|"
+    "0|{{video}}|0|0|0|{{diffs}}"  # 0s are threadid, has_story,
     # filesize, filesize_novid.
-).format(setid_spelling="SetId" if USING_CHIMU else "SetID")
+).format(setid_spelling="id")
 
 DIRECT_MAP_INFO_FMTSTR = (
-    "[{DifficultyRating:.2f}⭐] {DiffName} "
-    "{{cs: {CS} / od: {OD} / ar: {AR} / hp: {HP}}}@{Mode}"
+    "[{difficulty_rating:.2f}⭐] {version} "
+    "{{cs: {cs} / od: {accuracy} / ar: {ar} / hp: {drain}}}@{mode_int}"
 )
 
 
@@ -433,10 +433,7 @@ async def osuSearchHandler(
     mode: int = Query(..., alias="m", ge=-1, le=3),  # -1 for all
     page_num: int = Query(..., alias="p"),
 ):
-    if USING_CHIMU:
-        search_url = f"{app.settings.MIRROR_URL}/search"
-    else:
-        search_url = f"{app.settings.MIRROR_URL}/api/search"
+    search_url = f"{app.settings.MIRROR_URL}/api/v2/search"
 
     params: dict[str, object] = {"amount": 100, "offset": page_num * 100}
 
@@ -454,20 +451,12 @@ async def osuSearchHandler(
 
     async with app.state.services.http.get(search_url, params=params) as resp:
         if resp.status != status.HTTP_200_OK:
-            if USING_CHIMU:
-                # chimu uses 404 for no maps found
-                if resp.status == status.HTTP_404_NOT_FOUND:
-                    return b"0"
+            if resp.status == status.HTTP_404_NOT_FOUND:
+                return b"0"
 
             return b"-1\nFailed to retrieve data from the beatmap mirror."
 
         result = await resp.json()
-
-        if USING_CHIMU:
-            if result["code"] != 0:
-                return b"-1\nFailed to retrieve data from the beatmap mirror."
-
-            result = result["data"]
 
     lresult = len(result)  # send over 100 if we receive
     # 100 matches, so the client
@@ -475,18 +464,14 @@ async def osuSearchHandler(
     ret = [f"{'101' if lresult == 100 else lresult}"]
 
     for bmap in result:
-        if bmap["ChildrenBeatmaps"] is None:
+        if bmap["beatmaps"] is None:
             continue
 
-        if USING_CHIMU:
-            bmap["HasVideo"] = int(bmap["HasVideo"])
-        else:
-            # cheesegull doesn't support vids
-            bmap["HasVideo"] = "0"
+        bmap["video"] = int(bmap["video"])
 
         diff_sorted_maps = sorted(
-            bmap["ChildrenBeatmaps"],
-            key=lambda m: m["DifficultyRating"],
+            bmap["beatmaps"],
+            key=lambda m: m["difficulty_rating"],
         )
         diffs_str = ",".join(
             [DIRECT_MAP_INFO_FMTSTR.format(**row) for row in diff_sorted_maps],
@@ -1688,17 +1673,8 @@ async def get_osz(
     map_set_id: str = Path(...),
 ):
     """Handle a map download request (osu.ppy.sh/d/*)."""
-    no_video = map_set_id[-1] == "n"
-    if no_video:
-        map_set_id = map_set_id[:-1]
-
-    if USING_CHIMU:
-        query_str = f"download/{map_set_id}?n={int(not no_video)}"
-    else:
-        query_str = f"d/{map_set_id}"
-
     return RedirectResponse(
-        url=f"{app.settings.MIRROR_URL}/{query_str}",
+        url=f"{app.settings.MIRROR_URL}/d/{map_set_id}",
         status_code=status.HTTP_301_MOVED_PERMANENTLY,
     )
 
